c54fcc45691efa7d9df275c66fb9a43843d8a518
Szűcs Tibor <job.szucstibor@gmail.com>
2018-05-07 10:50:43 +0200

    Launcher class created

---
+++ src/main/java/org/personal/drawingsite/DrawingWebsiteLauncher.java
@@ -0,0 +1,12 @@
+package org.personal.drawingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawingWebsiteLauncher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrawingWebsiteLauncher.class, args);
+    }
+}
