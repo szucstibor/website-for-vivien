39dc95e0fa8af0d781443598f6b78ee0325cc711
Szűcs Tibor <job.szucstibor@gmail.com>
2018-05-07 10:52:47 +0200

    Basic repository created

--- src/main/java/org/personal/drawingsite/user/UserRepository.java
+++ src/main/java/org/personal/drawingsite/user/UserRepository.java
@@ -1,4 +1,7 @@
 package org.personal.drawingsite.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
