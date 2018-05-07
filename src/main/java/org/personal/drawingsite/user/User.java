da901f6b667414e0b5744fb76fb617ec3e1f72dc
Szűcs Tibor <job.szucstibor@gmail.com>
2018-05-07 10:51:52 +0200

    User class created

---
+++ src/main/java/org/personal/drawingsite/user/User.java
@@ -0,0 +1,55 @@
+package org.personal.drawingsite.user;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+// @SequenceGenerator(name = "seq", initialValue = 2, allocationSize = 2)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String emailAddress;
+
+    public User(){};
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
