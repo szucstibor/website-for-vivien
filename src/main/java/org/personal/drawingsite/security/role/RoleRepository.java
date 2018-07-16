1dfeaea09a66eee65f8c74d8440bdb38352363da
Szűcs Tibor <job.szucstibor@gmail.com>
2018-07-16 10:12:58 +0200

    Repository created for Role class

---
+++ src/main/java/org/personal/drawingsite/security/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package org.personal.drawingsite.security.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByRole(String role);
+}
