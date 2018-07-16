7ef259736fda0f6ec3f4bd92d01643cb3f32fa6c
Szűcs Tibor <job.szucstibor@gmail.com>
2018-07-16 09:57:42 +0200

    added more fields for easier control over the request

--- src/main/java/org/personal/drawingsite/drawRequest/DrawRequest.java
+++ src/main/java/org/personal/drawingsite/drawRequest/DrawRequest.java
@@ -6,22 +6,46 @@ import javax.persistence.*;
 @Table(name = "requests")
 public class DrawRequest {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "request_id")
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(name="name", nullable = false)
     private String requestName;
 
-    @Column(nullable = false, unique = true)
+    @Column(name="description", nullable = false)
     private String requestDescription;
 
-    @Column
+    @Column(name="complexity")
     private int complexity;
 
-    @Column
+    @Column(name="estimate")
     private int estimatedTime;
 
+    @Column(name = "accepted", nullable = false)
+    private boolean accepted;
+
+    @Column(name = "active", nullable = false)
+    private boolean active = false;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "done")
+    private boolean done = false;
+
+    public DrawRequest(String requestName, String requestDescription) {
+        this.requestName = requestName;
+        this.requestDescription = requestDescription;
+    }
+
     public DrawRequest() {
+    }
+
+    public DrawRequest(String requestName, String requestDescription, String username) {
+        this.requestName = requestName;
+        this.requestDescription = requestDescription;
+        this.username = username;
     }
 
     public Long getId() {
@@ -62,5 +86,37 @@ public class DrawRequest {
 
     public void setEstimatedTime(int estimatedTime) {
         this.estimatedTime = estimatedTime;
+    }
+
+    public boolean isAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        this.accepted = accepted;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean workingOn) {
+        this.active = workingOn;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
     }
 }
