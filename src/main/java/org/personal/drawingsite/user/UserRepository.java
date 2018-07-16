52a7391fde85cb45cc9fde4c3c65d9d2c2f3e3b1
Szűcs Tibor <job.szucstibor@gmail.com>
2018-07-16 10:01:37 +0200

    Created repository with find by email and username methods

--- src/main/java/org/personal/drawingsite/user/UserRepository.java
+++ src/main/java/org/personal/drawingsite/user/UserRepository.java
@@ -1,7 +1,12 @@
 package org.personal.drawingsite.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    User findByUserName(String username);
+
+    User findByEmail(String email);
 }
