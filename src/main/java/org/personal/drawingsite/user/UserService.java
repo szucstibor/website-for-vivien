018381641314ff4f20914be13e1717922b60536e
Szűcs Tibor <job.szucstibor@gmail.com>
2018-07-16 10:02:27 +0200

    Modified user class to have roles when registered

--- src/main/java/org/personal/drawingsite/user/UserService.java
+++ src/main/java/org/personal/drawingsite/user/UserService.java
@@ -1,26 +1,84 @@
 package org.personal.drawingsite.user;
 
+import org.personal.drawingsite.security.role.Role;
+import org.personal.drawingsite.security.role.RoleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import javax.servlet.http.HttpSession;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
-    UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     public UserService() {
     }
 
-    public void register(Map<String, String> requestParams){
-        String username = requestParams.get("regUsername");
-        String password = requestParams.get("regPassword");
-        String email = requestParams.get("email");
-        if (username.length() > 5 && password.length() > 6){
-            User newUser = new User(username, password, email);
-            userRepository.save(newUser);
+    public void register(UserRegistrationDto registeringUser) {
+        System.out.println("running register method");
+        String username = registeringUser.getUserName();
+        String password = passwordEncoder.encode(registeringUser.getPassword());
+        String email = registeringUser.getEmail();
+        User user = new User(username, password, email);
+        Role userRole = roleRepository.findByRole("USER");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+
+        userRepository.save(user);
+
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+
+        if (user == null) {
+            String message = "Username not found: " + username;
+            log.info(message);
+            throw new UsernameNotFoundException(message);
         }
+
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRole()))
+                .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(
+                username,
+                user.getPassword(),
+                true,
+                true, //Account Not Expired
+                true, //Credentials Not Expired
+                true,//Account Not Locked
+                authorities
+        ) {
+        };
+    }
+
+    public User findByUserName(String userName) {
+        return userRepository.findByUserName(userName);
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 }
