9446e5a3e988d89031005409f7366777e135b59b
Szűcs Tibor <job.szucstibor@gmail.com>
2018-07-16 10:11:49 +0200

    Image class created

---
+++ src/main/java/org/personal/drawingsite/image/Image.java
@@ -0,0 +1,70 @@
+package org.personal.drawingsite.image;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "iamges")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "complexity")
+    private int complexity;
+
+    @Column(name = "hours")
+    private int hours;
+
+    @Column(name = "name")
+    private String name;
+
+
+    public Image() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getComplexity() {
+        return complexity;
+    }
+
+    public void setComplexity(int complexity) {
+        this.complexity = complexity;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
