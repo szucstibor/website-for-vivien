19867c4cc848152616c2408d2dbc31997477cfc3
Szűcs Tibor <job.szucstibor@gmail.com>
2018-07-16 10:14:11 +0200

    Repository created for Image class

---
+++ src/main/java/org/personal/drawingsite/image/ImageRepository.java
@@ -0,0 +1,8 @@
+package org.personal.drawingsite.image;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+
+}
