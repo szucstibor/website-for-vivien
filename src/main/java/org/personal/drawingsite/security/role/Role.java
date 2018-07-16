df3347e959de052f02133724a6d4bedeea52ff11
Szűcs Tibor <job.szucstibor@gmail.com>
2018-07-16 10:12:41 +0200

    Role class created

---
+++ src/main/java/org/personal/drawingsite/security/role/Role.java
@@ -0,0 +1,41 @@
+package org.personal.drawingsite.security.role;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int id;
+
+    @Column(name = "role")
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+}
+
