91b3a681ca82e14123b09a2d86d1d8dfe264d8c4
Szűcs Tibor <job.szucstibor@gmail.com>
2018-07-16 10:01:10 +0200

    Created DTO for user reg

---
+++ src/main/java/org/personal/drawingsite/user/UserRegistrationDto.java
@@ -0,0 +1,46 @@
+package org.personal.drawingsite.user;
+
+
+public class UserRegistrationDto {
+
+    private String userName;
+
+    private String password;
+
+    private String matchingPassword;
+
+    private String email;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMatchingPassword() {
+        return matchingPassword;
+    }
+
+    public void setMatchingPassword(String matchingPassword) {
+        this.matchingPassword = matchingPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
